bde01ec9747c4d4df57307af5cfccc729517d707
avinash14331 <36401845+avinash14331@users.noreply.github.com>
2025-08-24 00:54:02 +0530

    Delete src directory

--- src/main/java/org/avi/shoppingcartmcp/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package org.avi.shoppingcartmcp.repository;
-
-import org.avi.shoppingcartmcp.entity.CartItem;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CartItemRepository extends MongoRepository<CartItem, String> {
-    CartItem findByProductId(String productId);
-    void deleteByProductId(String productId);
-}
