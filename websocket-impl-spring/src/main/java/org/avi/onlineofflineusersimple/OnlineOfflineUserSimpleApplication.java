03a4d79122c4d4880819242c9e62f3bddcbcc555
avinashkumar <avinash14331@gmail.com>
2025-09-11 13:18:13 +0530

    websocket sample app

---
+++ websocket-impl-spring/src/main/java/org/avi/onlineofflineusersimple/OnlineOfflineUserSimpleApplication.java
@@ -0,0 +1,13 @@
+package org.avi.onlineofflineusersimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOfflineUserSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineOfflineUserSimpleApplication.class, args);
+    }
+
+}
---
+++ websocket-impl-spring/src/test/java/org/avi/onlineofflineusersimple/OnlineOfflineUserSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.avi.onlineofflineusersimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineOfflineUserSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
